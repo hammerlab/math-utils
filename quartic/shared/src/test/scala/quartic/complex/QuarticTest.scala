b488caca6d7f4de63080dfd2005f93c222008de3
Ryan Williams <ryan.blake.williams@gmail.com>
2018-02-20 15:11:45 +0000

    rm dead quartic test code

--- quartic/shared/src/test/scala/quartic/complex/QuarticTest.scala
+++ quartic/shared/src/test/scala/quartic/complex/QuarticTest.scala
@@ -52,30 +52,4 @@ abstract class BigDecimalTest
    */
   ε = 1e-14
 
-/*
-  val M = 3
-  override val casePrintInterval = 1
-
-  implicit def solve(t: TestCase[D]): Seq[Complex[D]] = {
-    val Seq(a, b, c, d, e) = t.coeffs
-    Quartic.doubleComplex[D].apply(a, b, c, d, e)
-  }
-*/
-
-/*
-  test("bad") {
-    val result: Result[D] =
-      Result(
-        TestCase[D](
-          Seq(Real[D](3) → 2),
-          Seq(ImaginaryRootPair[D](-3, 1) → 1),
-          1
-        )
-      )
-
-    import Result.showResult
-
-    println((result → 1).show)
-  }
-*/
 }
