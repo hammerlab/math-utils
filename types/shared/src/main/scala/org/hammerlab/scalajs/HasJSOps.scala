a7d8a79991f074c9edb01f267e34b6d825a3ac09
Ryan Williams <ryan.blake.williams@gmail.com>
2018-03-04 02:53:48 +0000

    add js-predicate test

--- types/shared/src/main/scala/org/hammerlab/scalajs/HasJSOps.scala
+++ types/shared/src/main/scala/org/hammerlab/scalajs/HasJSOps.scala
@@ -6,6 +6,14 @@ trait HasJSOps {
 
 object HasJSOps
   extends HasJS {
+
+  /**
+   * For an arbitrary type, support using a different value in ScalaJS:
+   *
+   * {{{
+   * "%.11f".format(66380.788125) == "66380.78812500000" js "66380.78812500001"
+   * }}}
+   */
   implicit class JSOps[T](val l: T) extends AnyVal {
     def js(r: T): T = HasJSOps.jvm(l)(r)
   }
---
+++ types/shared/src/test/scala/org/hammerlab/scalajs/SyntaxTest.scala
@@ -0,0 +1,13 @@
+package org.hammerlab.scalajs
+
+class SyntaxTest
+  extends hammerlab.Suite {
+  test("imprecision") {
+    import hammerlab.scalajs._
+    // cf. https://github.com/scala-js/scala-js/issues/3202
+    ===(
+      "%.11f".format(66380.788125),
+      "66380.78812500000" js "66380.78812500001"
+    )
+  }
+}
